a1ceca2771252db3f59b9de3dab871d2726c7b39
Dominic Vonk <dominic@vonk.io>
2024-05-14 11:34:33 +0200

    Fix

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,42 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import { type NextAuthOptions } from "next-auth"
+import type { Provider } from "next-auth/providers"
+
+const prisma = new PrismaClient()
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    {
+      id: "zitadel",
+      name: "zitadel",
+      type: "oauth",
+      version: "2",
+      allowDangerousEmailAccountLinking: true,
+      wellKnown:
+        process.env.ZITADEL_ISSUER + "/.well-known/openid-configuration",
+      authorization: {
+        params: {
+          scope: `openid email profile offline_access urn:zitadel:iam:org:project:id:${process.env.ZITADEL_PROJECT_ID}:aud`,
+        },
+      },
+      profile: (profile) => {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        }
+      },
+      idToken: true,
+
+      checks: ["pkce", "state"],
+      client: {
+        token_endpoint_auth_method: "none",
+      },
+      // profile method and other stuff
+      clientId: process.env.ZITADEL_CLIENT_ID,
+    } satisfies Provider,
+  ],
+}
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,47 +1,8 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-import NextAuth, { type NextAuthOptions } from "next-auth"
-import type { Provider } from "next-auth/providers"
+import NextAuth from "next-auth"
 
-const prisma = new PrismaClient()
+import { authOptions } from "./options"
 
 export const dynamic = "force-dynamic"
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    {
-      id: "zitadel",
-      name: "zitadel",
-      type: "oauth",
-      version: "2",
-      allowDangerousEmailAccountLinking: true,
-      wellKnown:
-        process.env.ZITADEL_ISSUER + "/.well-known/openid-configuration",
-      authorization: {
-        params: {
-          scope: `openid email profile offline_access urn:zitadel:iam:org:project:id:${process.env.ZITADEL_PROJECT_ID}:aud`,
-        },
-      },
-      profile: (profile) => {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        }
-      },
-      idToken: true,
-
-      checks: ["pkce", "state"],
-      client: {
-        token_endpoint_auth_method: "none",
-      },
-      // profile method and other stuff
-      clientId: process.env.ZITADEL_CLIENT_ID,
-    } satisfies Provider,
-  ],
-}
 
 const handler = NextAuth(authOptions)
 
