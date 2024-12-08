9fc3c22bac14d10278ffefce13ef62287118fd8a
Dominic Vonk <dominic@vonk.io>
2024-12-08 11:54:18 +0100

    Implement token decryption and encryption for HomeyToken refresh process

--- app/api/roller/route.tsx
+++ app/api/roller/route.tsx
@@ -1,36 +1,49 @@
-import { PrismaClient } from "@prisma/client";
-import axios from "axios";
+import { PrismaClient } from "@prisma/client"
+import axios from "axios"
 
+import { decryptToken, encryptToken } from "@/lib/token-encryption"
 
-export const dynamic = 'force-dynamic';
-export async function GET (request: Request) {
-  if (process.env.ROLLER_KEY === request.headers.get('x-roller-key')) {
-    return new Response(JSON.stringify({}));
+export const dynamic = "force-dynamic"
+export async function GET(request: Request) {
+  if (process.env.ROLLER_KEY === request.headers.get("x-roller-key")) {
+    return new Response(JSON.stringify({}))
   }
-  const prisma = new PrismaClient();
-  const tokens = await prisma.homeyToken.findMany();
+  const prisma = new PrismaClient()
+  const tokens = await prisma.homeyToken.findMany()
   for (const token of tokens) {
     if (token.expires.getTime() < Date.now() - 60000) {
+      const decryptedRefreshToken = decryptToken(
+        token.refreshToken!,
+        token.userId!
+      )
       // refresh token
       // www token
-      const a = new URLSearchParams();
-      a.set('client_id', process.env.HOMEY_CLIENT_ID!)
-      a.set('client_secret', process.env.HOMEY_CLIENT_SECRET!)
-      a.set('grant_type', 'refresh_token')
-      a.set('refresh_token', token.refreshToken!)
+      const a = new URLSearchParams()
+      a.set("client_id", process.env.HOMEY_CLIENT_ID!)
+      a.set("client_secret", process.env.HOMEY_CLIENT_SECRET!)
+      a.set("grant_type", "refresh_token")
+      a.set("refresh_token", decryptedRefreshToken!)
 
-      const data = await axios.post('https://api.athom.com/oauth2/token', a.toString());
-      ;
+      const data = await axios.post(
+        "https://api.athom.com/oauth2/token",
+        a.toString()
+      )
       await prisma.homeyToken.update({
         where: {
-          id: token.id
+          id: token.id,
         },
         data: {
-          accessToken: data.data.access_token,
-          refreshToken: data.data.refresh_token,
-          expires: new Date((Date.now() + data.data.expires_in * 1000) - 1000)
-        }
-      });
+          accessToken: encryptToken(
+            data.data.access_token,
+            token.encryptionKey!
+          ),
+          refreshToken: encryptToken(
+            data.data.refresh_token,
+            token.encryptionKey!
+          ),
+          expires: new Date(Date.now() + data.data.expires_in * 1000 - 1000),
+        },
+      })
     }
   }
   return new Response(JSON.stringify({}))
